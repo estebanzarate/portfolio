06d13aadbf59f5c3f42b442a188759061f0a4661
Esteban Zárate <estebanzarateok@gmail.com>
2026-06-11 14:41:19 -0300

    update

--- src/data/profile.js
+++ src/data/profile.js
@@ -11,7 +11,7 @@ const profile = {
       htb: "https://app.hackthebox.com/users/1089152",
       thm: "https://tryhackme.com/p/no0funny",
       email: "mailto:estebanzarateok@gmail.com",
-      blog: "https://estebanzarate.gitbook.io/hackache",
+      blog: "https://hackache.vercel.app",
     },
     experience: [
       {
@@ -277,7 +277,7 @@ const profile = {
       htb: "https://app.hackthebox.com/users/1089152",
       thm: "https://tryhackme.com/p/no0funny",
       email: "mailto:estebanzarateok@gmail.com",
-      blog: "https://estebanzarate.gitbook.io/hackache",
+      blog: "https://hackache.vercel.app",
     },
     experience: [
       {
